691933a05eaf2f8ab97c236a90473b1a209cd716
Gorilazzz1 <92122305+Gorilazzz1@users.noreply.github.com>
2021-10-07 22:25:16 +0200

    Update ForLoop.java

--- src/test/java/ForLoop.java
+++ src/test/java/ForLoop.java
@@ -1,8 +1,8 @@
 public class ForLoop {
     public static void main(String[] args) {
-        /*for (int i = 0; i < 60; i=i+2){
+        for (int i = 0; i < 60; i=i+2){
             System.out.println(i);
-        }*/
+        }
 
         for (int i=0; i<10; i= i+1){
             System.out.println("test");
